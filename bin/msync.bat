43458d626f97eb1c7278d2d131ddc71db4eb13ae
MatteoLacki <matteo.lacki@gmail.com>
2020-10-14 16:07:59 +0100

    update bat files

--- bin/msync.bat
+++ bin/msync.bat
@@ -1,3 +1,3 @@
 net use M: \\msserver\majestix_rawdata
-python C:/Projects/fs_ops/bin/msync.py D:/Data/raw M:/RAW "*.d" --min_copy_hours 4 --min_delete_hours 48 --debug
+python C:/Projects/fs_ops/bin/msync.py D:/Data/raw M:/RAW "*.d" --min_copy_hours 4 --min_delete_hours 48 --debug --server_ip_port 192.168.1.100:9002
 pause
